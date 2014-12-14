85e572a67dfdc2d954731ebb37ce494188a106a4
ni-paa~ <fynjyx@gmail.com>
2014-12-14 02:01:59 +0200

    db 0.1

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,8 @@
 <?php
 /*Параметры для соединения с БД*/
 $mysql_host = "localhost";
-$mysql_user = "mysql";
-$mysql_pass = "mysql";
+$mysql_user = "root";
+$mysql_pass = "root";
 $mysql_db = "db";
 
 include ("inc/bd.php");		/*Подключаемся к серверу*/
